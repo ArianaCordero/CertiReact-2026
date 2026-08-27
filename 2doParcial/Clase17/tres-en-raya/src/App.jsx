28d6ed7a6873eb477576f3ca40127e0dda50c328
ArianaCordero <arianacorderom1@upb.edu>
2026-08-27 18:19:19 -0400

    Add Vite build workflow

--- 2doParcial/Clase17/tres-en-raya/src/App.jsx
+++ 2doParcial/Clase17/tres-en-raya/src/App.jsx
@@ -1,5 +1,7 @@
-import{ createRoot} from 'react-dom/client';
 import TresEnRaya from './TresEnRaya';
-const contenedor = document.getElementById('root');
-const root = createRoot(contenedor);
-root.render(<TresEnRaya />);
+
+function App() {
+  return <TresEnRaya />;
+}
+
+export default App;
